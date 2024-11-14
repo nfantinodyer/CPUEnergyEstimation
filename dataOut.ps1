95bafb227071febed84ff3219499e70f809c8c3f
Nicholas Fantino-Dyer <89417141+nfantinodyer@users.noreply.github.com>
2024-11-14 12:52:49 -0800

    Updated to add measurement and max run time
    Pushing data to influxDB

--- dataOut.ps1
+++ dataOut.ps1
@@ -1,19 +1,27 @@
-$outputFile = "dataOutput.csv"
+$outputFile = "dataOutputWithMeasurement.csv"
+$measurement = "pcm_data"
+$durationSeconds = 5 # Set the run duration to 5 seconds
+$startTime = Get-Date
 
-if(!(Test-Path $outputFile)){
-    "Timestamp,Counter,Value"|Out-File -FilePath $outputFile -Encoding utf8
+# Create the file with headers if it doesn't exist
+if (!(Test-Path $outputFile)) {
+    "Measurement,Timestamp,Counter,Value" | Out-File -FilePath $outputFile -Encoding utf8
 }
-$pcmCounterPaths=Get-Counter -ListSet "PCM*" | ForEach-Object {$_.Paths}
 
-while($true){
-    $pcmCounters=Get-Counter -Counter $pcmCounterPaths
-    $timestamp=(Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+# Get the list of PCM counter paths
+$pcmCounterPaths = Get-Counter -ListSet "PCM*" | ForEach-Object { $_.Paths }
 
-    foreach($counter in $pcmCounters.CounterSamples){
-        $counterPath= $counter.Path.Replace("\","_").Replace(" ","_")
-        $value=$counter.CookedValue
-        "$timestamp,$counterPath,$value" | Out-File -FilePath $outputFile -Append -Encoding utf8
+# Start the loop to capture counter data
+while ((Get-Date) -lt $startTime.AddSeconds($durationSeconds)) {
+    $pcmCounters = Get-Counter -Counter $pcmCounterPaths
+    $timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+
+    foreach ($counter in $pcmCounters.CounterSamples) {
+        $counterPath = $counter.Path.Replace("\", "_").Replace(" ", "_")
+        $value = $counter.CookedValue
+        # Prepend the measurement to each line
+        "$measurement,$timestamp,$counterPath,$value" | Out-File -FilePath $outputFile -Append -Encoding utf8
     }
 
-    Start-Sleep -Seconds 10
+    Start-Sleep -Milliseconds 250
 }
