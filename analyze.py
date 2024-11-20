a75d3afa39904f9c2af3b97ab19fc0ee86ac7684
Nicholas Fantino-Dyer <2013RECK@gmail.com>
2024-11-19 20:52:51 -0800

    CorrelationMatrix

--- analyze.py
+++ analyze.py
@@ -15,14 +15,12 @@ windows ='Data/newWindows.csv'
 ##datatype measurement,dateTime:2006-01-02T15:04:05.000Z,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double,double
 #WindowsPCM,DateTime,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,READ,WRITE,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,C0res%,C1res%,C3res%,C6res%,C7res%,C0res%,C2res%,C3res%,C6res%,C7res%,C8res%,C9res%,C10res%,Proc Energy (Joules),Power Plane 0 Energy (Joules),Power Plane 1 Energy (Joules),EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,READ,WRITE,IO,IA,GT,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,C0res%,C1res%,C3res%,C6res%,C7res%,C0res%,C2res%,C3res%,C6res%,C7res%,C8res%,C9res%,C10res%,SKT0,SKT0,SKT0,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,C0res%,C1res%,C3res%,C6res%,C7res%,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,C0res%,C1res%,C3res%,C6res%,C7res%,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,C0res%,C1res%,C3res%,C6res%,C7res%,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,C0res%,C1res%,C3res%,C6res%,C7res%,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,C0res%,C1res%,C3res%,C6res%,C7res%,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,C0res%,C1res%,C3res%,C6res%,C7res%,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,C0res%,C1res%,C3res%,C6res%,C7res%,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,EXEC,IPC,FREQ,AFREQ,CFREQ,L3MISS,L2MISS,L3HIT,L3MPI,L2MPI,C0res%,C1res%,C3res%,C6res%,C7res%,TEMP,INST,ACYC,TIME(ticks),PhysIPC,PhysIPC%,INSTnom,INSTnom%,
 
-#Load data
 linux_data = pandas.read_csv(linux, skiprows=1)
 windows_data = pandas.read_csv(windows, skiprows=1)
 
 linux_data['DateTime'] = pandas.to_datetime(linux_data['DateTime'])
 windows_data['DateTime'] = pandas.to_datetime(windows_data['DateTime'])
 
-# Normalize DateTime columns to start from the same point
 min_datetime = min(linux_data['DateTime'].min(), windows_data['DateTime'].min())
 linux_data['NormalizedDateTime'] = linux_data['DateTime'] - linux_data['DateTime'].min()
 windows_data['NormalizedDateTime'] = windows_data['DateTime'] - windows_data['DateTime'].min()
@@ -31,21 +29,47 @@ windows_data['NormalizedDateTime'] = windows_data['DateTime'] - windows_data['Da
 linux_data['ShiftedDateTime'] = linux_data['NormalizedDateTime'] + min_datetime
 windows_data['ShiftedDateTime'] = windows_data['NormalizedDateTime'] + min_datetime
 
+linux_columns_of_interest = [
+    'Proc Energy (Joules)', 'L3MISS', 'L2MISS', 'L3HIT', 'L2HIT', 'EXEC', 'IPC', 'FREQ', 'AFREQ', 'CFREQ',
+    'TEMP', 'INSTnom', 'INSTnom%','C0res%', 'C1res%', 'C3res%', 'C6res%', 'C7res%', 'READ', 'WRITE', 'L3MPI', 'L2MPI', 
+    'TIME(ticks)', 'PhysIPC', 'PhysIPC%', 'SKT0'
+]
 
-# Plot data
-plt.figure(figsize=(10, 5))
+windows_columns_of_interest = [
+    'Proc Energy (Joules)', 'L3MISS', 'L2MISS', 'L3HIT', 'EXEC', 'IPC', 'FREQ', 'AFREQ', 'CFREQ',
+    'TEMP', 'INSTnom', 'INSTnom%','C0res%', 'C1res%', 'C3res%', 'C7res%', 'READ', 'WRITE', 'L3MPI', 'L2MPI', 
+    'TIME(ticks)', 'PhysIPC', 'PhysIPC%', 'SKT0'
+]
 
-# Plot Linux energy usage
-plt.plot(linux_data['ShiftedDateTime'], linux_data['Proc Energy (Joules)'], label='Linux Energy Usage')
 
-# Plot Windows energy usage
-plt.plot(windows_data['ShiftedDateTime'], windows_data['Proc Energy (Joules)'], label='Windows Energy Usage')
+linux_data_filtered = linux_data[[col for col in linux_columns_of_interest if col in linux_data.columns]]
+windows_data_filtered = windows_data[[col for col in windows_columns_of_interest if col in windows_data.columns]]
 
-# Add labels and title
-plt.xlabel('Normalized DateTime')
-plt.ylabel('Energy Usage (Joules)')
-plt.title('Energy Usage over Time')
-plt.legend()
+linux_corr = linux_data_filtered.corr()
+windows_corr = windows_data_filtered.corr()
 
-# Show plot
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.title('Linux Correlation Matrix')
+plt.imshow(linux_corr, cmap='coolwarm', interpolation='none')
+plt.colorbar()
+plt.xticks(range(len(linux_corr.columns)), linux_corr.columns, rotation=90)
+plt.yticks(range(len(linux_corr.columns)), linux_corr.columns)
+
+plt.subplot(1, 2, 2)
+plt.title('Windows Correlation Matrix')
+plt.imshow(windows_corr, cmap='coolwarm', interpolation='none')
+plt.colorbar()
+plt.xticks(range(len(windows_corr.columns)), windows_corr.columns, rotation=90)
+plt.yticks(range(len(windows_corr.columns)), windows_corr.columns)
+
+plt.tight_layout()
 plt.show()
+
+# Print correlation with energy usage
+print("Linux Correlation with Energy Usage:")
+print(linux_corr['Proc Energy (Joules)'])
+
+print("\nWindows Correlation with Energy Usage:")
+print(windows_corr['Proc Energy (Joules)'])
