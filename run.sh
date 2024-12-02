d6816a18fe3772ad8a949f9cccf7a2056e2dff35
Nicholas Fantino-Dyer <2013RECK@gmail.com>
2024-12-01 21:01:00 -0800

    correct dirs

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Directories
-PCM_DIR="/home/yourusername/Desktop/pcm/build/bin"
-OUTPUT_DIR="/home/yourusername/Desktop/Data"
+PCM_DIR="Desktop/pcm/build/bin"
+OUTPUT_DIR="Desktop/Data"
 
 # Create output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
