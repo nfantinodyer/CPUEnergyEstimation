a331d0b1071ab0995b14102fb5dcba2a80b6c879
Nicholas Fantino-Dyer <2013RECK@gmail.com>
2024-12-01 22:56:05 -0800

    fixed brackets

--- expectScript.sh
+++ expectScript.sh
@@ -6,7 +6,7 @@
 set PCM_DIR "/home/rob/Desktop/pcm/build/bin"
 set PCM_SAMPLING_INTERVAL "1"
 set PCM_COUNT "60"
-set OUTPUT_FILE "/home/rob/Desktop/Data/Linux${env(LOAD_PERCENT)}Static${env(THREAD_LABEL)}.csv"
+set OUTPUT_FILE "/home/rob/Desktop/Data/Linux$env(LOAD_PERCENT)Static$env(THREAD_LABEL).csv"
 
 # Run pcm with sudo
 spawn sudo "$PCM_DIR/pcm" /csv "$PCM_SAMPLING_INTERVAL" "$PCM_COUNT"
